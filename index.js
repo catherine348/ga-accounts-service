c6de2b89ab1b6d69f860abb7ee20da01ac08bc0e
catherine russell <ME\du_348518-1781263435@a-rc1gbxrrwjpp.me.ga.co>
2026-07-21 12:22:54 +0100

    built app up to end of first session router has routes for /accounts and /accounts:id, contoller functions are in place in contoller file

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const port = process.env.PORT;
+import express from "express";
+import Router from "./views/router.js";
 
-console.log(port);
+const port = process.env.PORT;
+const app = express();
+
+app.use(express.json());
+app.use(Router);
+
+async function startServer() {
+  try {
+    app.listen(port, () => console.log(`🤖 Listening on Port: ${port}`));
+  } catch (err) {
+    console.log("🤖 Oh no something went wrong", err);
+  }
+}
+
+startServer();
